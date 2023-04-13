34a1111bb2506a735c821350ff9db8be5aefcdac
jeyofdev <jgregoire.pro@gmail.com>
2023-04-13 15:58:26 +0200

    config : fix eslint imports config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		console.log('ok');
+	}
 }
